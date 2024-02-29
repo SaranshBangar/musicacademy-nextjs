c580b971c5f2c26722f062e1338837f74215c7f6
Saransh Bangar <saranshbangad@gmail.com>
2024-02-29 09:16:12 +0530

    Added all the initial files

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { Meteors } from "@/components/ui/meteors"
+
+function page() {
+  return (
+    <div className="min-h-screen bg-black py-12 pt-36">
+        <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">Contact Us</h1>
+        <Meteors />
+        <p className="text-center text-2xl mt-20">Coming soon!</p>
+    </div>
+  )
+}
+
+export default page
